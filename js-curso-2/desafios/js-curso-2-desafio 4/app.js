0d39dd0a4d1a978d61c0eac72537b9f8a7a780f8
LeonardoWr <leo.wrightlima@gmail.com>
2024-06-24 20:42:32 -0300

    Desafio: hora da prática 4

---
+++ js-curso-2/desafios/js-curso-2-desafio 4/app.js
@@ -0,0 +1,15 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio';
+
+let listaGenerica = [];
+
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('GoLang');
+
+let nomes = ['Jeremias', 'Rubião', 'Gostavo'];
+console.log(nomes[0]);
+
+console.log(nomes[nomes.length - 1]);
